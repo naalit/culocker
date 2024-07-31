a11236b2f3a0923b15b3920c2c9e4e14481fb634
Sylvie Dirkswager <sylvie.dirkswager@gmail.com>
2024-07-31 11:46:35 -0500

    Add CDF plot

--- plot2.py
+++ plot2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Build combined dataframe for both tasks
 df_long = pd.read_csv("out/cu_log_T3_L3.csv")
@@ -10,12 +11,37 @@ df_short['task'] = 'short'
 df_long['time'] = df_long['time'].map(lambda x: x * 10) # make the scales more comparable. still not ideal since the scales are different (ms vs ds) but we'll figure something out
 df = pd.concat([df_short, df_long])
 
-# Plot it
+# Box plot
 plt.figure(figsize=(12, 6))
 sns.boxplot(x='window_size', y='time', hue='task', data=df)
 plt.title('Time by Window Size and Task')
 plt.xlabel('Window Size')
 plt.ylabel('Time')
+plt.show()
+
+# CDF plot
+tasks = ['short', 'long']
+window_sizes = sorted(df['window_size'].unique())
+colors = plt.cm.rainbow(np.linspace(0, 1, len(window_sizes)))
+
+fig, axs = plt.subplots(len(tasks), 1, figsize=(12, 4*len(tasks)), sharex=True)
+fig.suptitle('Cumulative Distribution Function of Time by Task and Window Size')
+
+for i, task in enumerate(tasks):
+    for window_size, color in zip(window_sizes, colors):
+        task_data = df[(df['task'] == task) & (df['window_size'] == window_size)]['time']
+        if len(task_data) > 0:
+            x = np.sort(task_data)
+            y = np.arange(1, len(x) + 1) / len(x)
+            axs[i].plot(x, y, color=color, label=f'Window Size {window_size}')
+    
+    axs[i].set_ylabel(f'Task {task}')
+    axs[i].grid(True)
+    axs[i].legend()
+
+plt.xlabel('Time')
+fig.text(0.01, 0.5, 'Cumulative Probability', va='center', rotation='vertical')
+plt.tight_layout()
 plt.show()
 
 # Calculate quantiles
